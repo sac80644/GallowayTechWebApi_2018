ae5339dc79b61c6ecb2e538364197cf75711fa2b
sac80644 <jason@gallowaytech.com>
2018-06-23 23:40:38 -0500

    Made some OAuthAuthorizationServerOptions configurable.

--- GallowayTechWebApi_2018/App_Start/Startup.Auth.cs
+++ GallowayTechWebApi_2018/App_Start/Startup.Auth.cs
@@ -1,15 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
-//using Microsoft.Owin.Security.Google;
 using Microsoft.Owin.Security.OAuth;
 using Owin;
 using GallowayTechWebApi_2018.Providers;
 using GallowayTechWebApi_2018.Models;
+using System.Web.Configuration;
+using System.Text.RegularExpressions;
 
 namespace GallowayTechWebApi_2018
 {
@@ -31,16 +29,28 @@ namespace GallowayTechWebApi_2018
             app.UseCookieAuthentication(new CookieAuthenticationOptions());
             app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
 
+            //Set Token expiration from Web.config
+            var isNumeric = int.TryParse(WebConfigurationManager.AppSettings["TokenExpireTimeSpanFromMinutes"], out int tokenExpireTimeSpanFromMinutes);
+            if(!isNumeric) { tokenExpireTimeSpanFromMinutes = 20; }
+
+            //AllowInsecureHttp from Web.config
+            var isBoolean = bool.TryParse(WebConfigurationManager.AppSettings["AllowInsecureHttp"], out bool allowInsecureHttp);
+            if(!isBoolean) { allowInsecureHttp = true; }
+
+            //TokenEndpointPath from Web.config - default is Token
+            var tokenEndpointPath = WebConfigurationManager.AppSettings["TokenEndpointPath"];
+            if (!Regex.IsMatch(tokenEndpointPath, @"^[a-zA-Z]+$")) { tokenEndpointPath = "Token"; }
+            
             // Configure the application for OAuth based flow
             PublicClientId = "self";
             OAuthOptions = new OAuthAuthorizationServerOptions
             {
-                TokenEndpointPath = new PathString("/Token"),
+                TokenEndpointPath = new PathString("/" + tokenEndpointPath),
                 Provider = new ApplicationOAuthProvider(PublicClientId),
                 AuthorizeEndpointPath = new PathString("/api/Account/ExternalLogin"),
-                AccessTokenExpireTimeSpan = TimeSpan.FromDays(14),
+                AccessTokenExpireTimeSpan = TimeSpan.FromMinutes(tokenExpireTimeSpanFromMinutes),
                 // In production mode set AllowInsecureHttp = false
-                AllowInsecureHttp = true
+                AllowInsecureHttp = allowInsecureHttp
             };
 
             // Enable the application to use bearer tokens to authenticate users
