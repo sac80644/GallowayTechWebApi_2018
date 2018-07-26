137154c8273ded3c10bfb60053a305d4d25381a6
Jason Galloway <jason@gallowaytech.com>
2018-07-26 13:52:40 -0500

    Update AlbumsInsertScript.sql

--- Database Scripts/AlbumsInsertScript.sql
+++ Database Scripts/AlbumsInsertScript.sql
@@ -2,9 +2,9 @@ USE [GallowayTechDB]
 GO
 
 INSERT INTO Albums
-VALUES ('2010 - Mascots Album', 1, getdate(), getdate()),
-('2007 - Peru	1', 1, getdate(), getdate()),
-('2011 - San Francisco Fleet Week', 1, getdate(), getdate()),
-('2012 - Death Valley (In Progress)', 1, getdate(), getdate())
+VALUES ('2010 - Mascots Album', '2010 - Mascots Album', 'Buddy Tacoma',  1, getdate(), getdate()),
+('2007 - Peru	1', '2007 - Peru	1', '', 1, getdate(), getdate()),
+('2011 - San Francisco Fleet Week', '2011 - San Francisco Fleet Week', '', 1, getdate(), getdate()),
+('2012 - Death Valley (In Progress)', '2012 - Death Valley (In Progress)', '', 1, getdate(), getdate())
 
 SELECT * FROM Albums
