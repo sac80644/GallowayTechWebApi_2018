4f04c84fdd34358a99d6ab550d8033b908b6c62c
Jason Galloway <jason@gallowaytech.com>
2018-08-12 23:40:23 -0500

    Update AlbumsInsertScript.sql
    Updating insert script for Albums

--- GallowayTechWebApi_2018/Database/DML/AlbumsInsertScript.sql
+++ GallowayTechWebApi_2018/Database/DML/AlbumsInsertScript.sql
@@ -2,9 +2,10 @@ USE [GallowayTechDB]
 GO
 
 INSERT INTO Albums
-VALUES ('2010 - Mascots Album', '2010 - Mascots Album', 'Buddy Tacoma',  1, getdate(), getdate()),
-('2007 - Peru	1', '2007 - Peru	1', '', 1, getdate(), getdate()),
-('2011 - San Francisco Fleet Week', '2011 - San Francisco Fleet Week', '', 1, getdate(), getdate()),
-('2012 - Death Valley (In Progress)', '2012 - Death Valley (In Progress)', '', 1, getdate(), getdate())
+VALUES ('2010 - Mascots Album', '2010 - Mascots Album', 'We no longer have a cat...Our dog Tacoma passed away some years ago but Buddy has been a great dog.', 'Buddy Tacoma', 1, GETDATE(), GETDATE()),
+('2007 - Peru',	'2007 - Peru', 'My wife, brother and I went to Peru and met up with my sister in law.  We visited several sites including Machu Pichu.', 'Peru Machu Pichu', 1, GETDATE(), GETDATE()),
+('2011 - San Francisco Fleet Week', '2011 - San Francisco Fleet Week', 'We were able to watch the air show in San Francisco aboard the USS O''Brien.', 'Air Show', 1, GETDATE(), GETDATE()),
+('2012 - Death Valley', '2012 - Death Valley', 'Another trip where my wife and I went with her sister.  Death Valley was actually pretty cool.', 'Death Valley', 1, GETDATE(), GETDATE())
 
 SELECT * FROM Albums
+
